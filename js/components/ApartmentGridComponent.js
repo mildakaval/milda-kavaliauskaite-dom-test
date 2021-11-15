ca64ab7048360daa4dd94d5d6b5625296c61c274
Milda Kavaliauskaite <mildak22@gmail.com>
2021-11-15 21:22:50 +0200

    6: nustatytas siuntimas

--- js/components/ApartmentGridComponent.js
+++ js/components/ApartmentGridComponent.js
@@ -1,9 +1,33 @@
 class ApartmentGridComponent {
     constructor(){
+        this.state = {
+            loading: false,
+            apartment: []
+        }
         this.init();
     }
+    fetchApartment = () => API.fetchApartment(this.saveApartment);
+    saveApartment = (apartment) => {
+        this.state.apartment = apartment;
+        this.state.loading = false;
+
+        this.render();
+    }
+
     init = () => {
+        this.state.loading = true;
+        this.fetchApartment();
         this.htmlElement = document.createElement('div');
-        this.htmlElement.innerHTML = 'Aš - ApartmentGridComponent';
+
+        this.render();
+    }
+
+    render = () => {
+        const { loading, cars } = this.state;
+        if (loading) {
+            this.htmlElement.innerHTML = 'Siunčiama.....';
+        } else {
+            this.htmlElement.innerHTML = 'Parsiųsta!';
+        }
     }
 }
